2e8fd047d6892a5fd7894a9bd4ceaa8323b43878
Jeff Mead <jeffdmead171@gmail.com>
2020-10-08 10:01:21 -0400

    updated service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,11 +3,10 @@ const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 
 const FILES_TO_CACHE = [
-  "index.html",
-  "css/style.css",
-  "js/index.js",
-  "js/idb.js",
-  "./models/transaction.js"
+  "./index.html",
+  "./css/styles.css",
+  "./js/index.js",
+  "./js/idb.js"
 ];
 
 
@@ -51,9 +50,6 @@ self.addEventListener('fetch', function (e) {
         console.log('file is not cached, fetching : ' + e.request.url)
         return fetch(e.request)
       }
-
-      // You can omit if/else for console.log & put one line below like this too.
-      // return request || fetch(e.request)
     })
   )
 })
