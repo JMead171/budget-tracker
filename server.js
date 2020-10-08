b9e4fea28c832779ac0948e38a579a2d13053e8d
Jeff Mead <jeffdmead171@gmail.com>
2020-10-08 12:35:04 -0400

    updated server file

--- server.js
+++ server.js
@@ -16,16 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget-tracker', {
-  useFindAndModify: false,
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useNewUrlParser: true,
+  useFindAndModify: false
 });
-
-// mongoose.connect(MONGODB_URI, {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
 
 // routes
 app.use(require("./routes/api.js"));
